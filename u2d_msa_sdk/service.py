c46d6279496d2506fa5600fdacf673c77cfe4ea0
swelcker <swelcker@expertsystem.com>
2022-08-23 18:55:48 +0300

    Initial Creation

--- u2d_msa_sdk/service.py
+++ u2d_msa_sdk/service.py
@@ -1,11 +1,14 @@
 import os
-from typing import Optional, Union
+from typing import Optional, Union, List
 
 import httpx
 from fastapi_users import BaseUserManager, UUIDIDMixin, FastAPIUsers, InvalidPasswordException, models
+from fastapi_users.models import UP, ID
 from fastapi_users.password import PasswordHelper
 from prometheus_fastapi_instrumentator import Instrumentator
 from pydantic import Json
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
@@ -14,19 +17,53 @@ from u2d_msa_sdk.msaapi import MSAFastAPI
 from u2d_msa_sdk.models.service import MSAServiceDefinition, MSAHealthDefinition
 from passlib.context import CryptContext
 import uvloop
-
+from starlette_wtf import CSRFProtectMiddleware
+from starception import StarceptionMiddleware
 from u2d_msa_sdk.security import getMSASecurity
 
 context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 password_helper = PasswordHelper(context)
 security = getMSASecurity()
 
+
 def getSecretKey():
-    ret_key: str = os.getenv("SECRET_KEY_TOKEN", "u2dmsaservicex_#M8A{1o3Bd?<ipwt^K},Z)OE<Fkj-X9IILWq|Cf`Y:HFI~&2L%Ion3}+p{T%")
+    ret_key: str = os.getenv("SECRET_KEY_TOKEN",
+                             "u2dmsaservicex_#M8A{1o3Bd?<ipwt^K},Z)OE<Fkj-X9IILWq|Cf`Y:HFI~&2L%Ion3}+p{T%")
     return ret_key
 
 
-fastapi_users: FastAPIUsers = FastAPIUsers[User, uuid.UUID](get_user_manager, security.auth_backends)
+def getSecretKeySessions():
+    ret_key: str = os.getenv("SECRET_KEY_SESSIONS",
+                             "u2dmsaserviceeP)zg5<g@4WJ0W8'?ad!T9UBvW1z2k|y~|Pgtewv=H?GY_Q]t~-~UUe'pJ0V[>!<)")
+    return ret_key
+
+
+def getSecretKeyCSRF():
+    ret_key: str = os.getenv("SECRET_KEY_CSRF",
+                             "u2dmsaservicee_rJM'onkEV1trD=I7dci$flB)aSNW+raL4j]Ww=n~_BRg35*3~(E.>rx`1aTw:s")
+    return ret_key
+
+
+def getAllowedOrigins() -> List[str]:
+    origins: List[str] = [os.getenv("ALLOWED_ORIGINS", "*")]
+    return origins
+
+
+def getAllowedMethods() -> List[str]:
+    methods: List[str] = [os.getenv("ALLOWED_METHODS", "*")]
+    return methods
+
+
+def getAllowedHeaders() -> List[str]:
+    headers: List[str] = [os.getenv("ALLOWED_HEADERS", "*")]
+    return headers
+
+
+def getAllowedCredentials() -> bool:
+    cred: bool = os.getenv("ALLOWED_CREDENTIALS", True)
+    return cred
+
+fastapi_users: FastAPIUsers = FastAPIUsers(BaseUserManager[UP, ID], security.auth_backends)
 
 
 class NoMasterService(Exception):
@@ -66,6 +103,15 @@ class MSAApp(MSAFastAPI):
         self.include_router(
             fastapi_users.get_auth_router(security.auth_backend_cookie), prefix="/auth/jwt", tags=["auth"]
         )
+        self.add_middleware(StarceptionMiddleware)
+        self.add_middleware(CORSMiddleware, allow_origins=getAllowedOrigins(),
+                           allow_credentials=getAllowedCredentials(),
+                           allow_methods=getAllowedMethods(),
+                           allow_headers=getAllowedHeaders(), )
+        # app.add_middleware(HTTPSRedirectMiddleware)
+        # app.add_middleware(GZipMiddleware)
+        self.add_middleware(SessionMiddleware, secret_key=getSecretKeySessions())
+        self.add_middleware(CSRFProtectMiddleware, csrf_secret=getSecretKeyCSRF())
 
         if self.is_master:
             Instrumentator().instrument(self).expose(self)
